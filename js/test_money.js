6452c3cf522e79cf43b03cd462880d165799929e
dinkargupta <gupta.dinkar@gmail.com>
2021-12-27 16:11:49 +0100

    feature: Euro conversion

--- js/test_money.js
+++ js/test_money.js
@@ -10,6 +10,22 @@ class Dollar {
     }
 }
 
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency
+    }
+
+    times(multipler) {
+        return new Money(this.amount * multipler, this.currency);
+    }
+}
+
 let fiver = new Dollar(5);
 let tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 10);
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuroes = tenEuros.times(2);
+assert.strictEqual(twentyEuroes.amount, 20);
+assert.strictEqual(twentyEuroes.currency, "EUR");
